6cc227a5512d8ca6dcf5e3bce6cc13810acba9fa
Adama SEYE <a.seye3777@zig.univ.sn>
2026-01-03 13:46:37 +0100

    change base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:true,
-    apiUrl: 'http://backend.example.com'
+    apiUrl: 'https://api-backend.election.com'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:false,
-    apiUrl: 'http://backend.example.com'
+    apiUrl: 'https://api-backend.election.com'
   };
